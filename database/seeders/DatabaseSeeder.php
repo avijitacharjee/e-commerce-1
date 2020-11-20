ff0a4f805e659b5feb5f6cec1bdfb6e568887746
avijitach@gmail.com <avijitach@gmail.com>
2020-11-20 14:08:09 +0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        
+        User::factory(29)->create();
     }
 }
